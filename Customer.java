428da9cc2b3926f626b585db5a8c7f8b6c4481fc
Aijan01 <55388198+Aijan01@users.noreply.github.com>
2019-10-27 18:37:59 +0600

    Delete Customer.java

--- Customer.java
+++
@@ -1,8 +0,0 @@
-public class Customer
-{
-    public static void main(String[] args)
-    {
-        Shop shop = new Shop(48641233L, "apple", 185.00,"vegetablesFruits", "12.12.2019");
-        System.out.println(shop.buyProduct());
-    }
-}
